ab0bd929415519e49882171467a1683207667f24
Scott Bassin <scott.bassin@ibotta.com>
2016-08-11 09:48:38 -0600

    Revert "Taylor Kaplan's submission."
    This reverts commit 355b3b2080289002d500651f6f5c8d2d67879c6c.

--- platform_dev/anagram_test.rb
+++ platform_dev/anagram_test.rb
@@ -13,24 +13,26 @@ class TestCases < Test::Unit::TestCase
     @client = AnagramClient.new(ARGV)
 
     # add words to the dictionary
-    @client.post('/words', nil, {"words" => ["read", "dear", "dare"] }) rescue nil
+    @client.post('/words.json', nil, {"words" => ["read", "dear", "dare"] }) rescue nil
   end
 
   # runs after each test
   def teardown
     # delete everything
-    @client.delete('/words') rescue nil
+    @client.delete('/words.json') rescue nil
   end
 
   def test_adding_words
-    res = @client.post('/words', nil, {"words" => ["read", "dear", "dare"] })
+    res = @client.post('/words.json', nil, {"words" => ["read", "dear", "dare"] })
 
     assert_equal('201', res.code, "Unexpected response code")
   end
 
   def test_fetching_anagrams
+    pend # delete me
+
     # fetch anagrams
-    res = @client.get('/anagrams/read')
+    res = @client.get('/anagrams/read.json')
 
     assert_equal('200', res.code, "Unexpected response code")
     assert_not_nil(res.body)
@@ -44,8 +46,10 @@ class TestCases < Test::Unit::TestCase
   end
 
   def test_fetching_anagrams_with_limit
+    pend # delete me
+
     # fetch anagrams with limit
-    res = @client.get('/anagrams/read', 'limit=1')
+    res = @client.get('/anagrams/read.json', 'limit=1')
 
     assert_equal('200', res.code, "Unexpected response code")
 
@@ -55,8 +59,10 @@ class TestCases < Test::Unit::TestCase
   end
 
   def test_fetch_for_word_with_no_anagrams
+    pend # delete me
+
     # fetch anagrams with limit
-    res = @client.get('/anagrams/zyxwv')
+    res = @client.get('/anagrams/zyxwv.json')
 
     assert_equal('200', res.code, "Unexpected response code")
 
@@ -66,12 +72,14 @@ class TestCases < Test::Unit::TestCase
   end
 
   def test_deleting_all_words
-    res = @client.delete('/words')
+    pend # delete me
+
+    res = @client.delete('/words.json')
 
     assert_equal('204', res.code, "Unexpected response code")
 
     # should fetch an empty body
-    res = @client.get('/anagrams/read')
+    res = @client.get('/anagrams/read.json')
 
     assert_equal('200', res.code, "Unexpected response code")
 
@@ -81,14 +89,16 @@ class TestCases < Test::Unit::TestCase
   end
 
   def test_deleting_all_words_multiple_times
+    pend # delete me
+
     3.times do
-      res = @client.delete('/words')
+      res = @client.delete('/words.json')
 
       assert_equal('204', res.code, "Unexpected response code")
     end
 
     # should fetch an empty body
-    res = @client.get('/anagrams/read', 'limit=1')
+    res = @client.get('/anagrams/read.json', 'limit=1')
 
     assert_equal('200', res.code, "Unexpected response code")
 
@@ -98,65 +108,20 @@ class TestCases < Test::Unit::TestCase
   end
 
   def test_deleting_single_word
+    pend # delete me
+
     # delete the word
-    res = @client.delete('/words/dear')
+    res = @client.delete('/words/dear.json')
 
     assert_equal('200', res.code, "Unexpected response code")
 
     # expect it not to show up in results
-    res = @client.get('/anagrams/read')
+    res = @client.get('/anagrams/read.json')
 
     assert_equal('200', res.code, "Unexpected response code")
 
     body = JSON.parse(res.body)
 
     assert_equal(['dare'], body['anagrams'])
-  end
-
-
-# **Optional**
-# - Endpoint that returns a count of words in the corpus and min/max/median/average word length
-# - Respect a query param for whether or not to include proper nouns in the list of anagrams
-# - Endpoint that identifies words with the most anagrams
-# - Endpoint that takes a set of words and returns whether or not they are all anagrams of each other
-# - Endpoint to return all anagram groups of size >= *x*
-# - Endpoint to delete a word *and all of its anagrams*
-
-# - Endpoint that returns a count of words in the corpus and min/max/median/average word length
-  def test_word_stats
-    res = @client.get('/stats')
-
-    assert_equal('200', res.code, "Unexpected response code")
-
-    stats = JSON.parse(res.body)['stats']
-
-    assert_equal({
-      "total_count" => 3,
-      "min" => 4,
-      "max" => 4,
-      "median"=>4,
-      "average" => 4,
-      "weight" => 12,
-      "word_length_hash" => {
-        "4" => 3
-      }
-    }, stats)
-  end
-
-# - Respect a query param for whether or not to include proper nouns in the list of anagrams
-  def test_proper_nouns
-    # Taylor is word in the dictionary, so it is not a proper noun
-    # aylort and loryat however are not so they are considered proper nouns
-    @client.post('/words', nil, {"words" => ["taylor","aylort","loryat"] }) rescue nil
-
-    res = @client.get('/anagrams/loryat',"properNoun=true")
-
-    assert_equal('200', res.code, "Unexpected response code")
-    assert_not_nil(res.body)
-
-    body = JSON.parse(res.body)
-
-    expected_anagrams = %w(aylort)
-    assert_equal(expected_anagrams, body['anagrams'].sort)
   end
 end
