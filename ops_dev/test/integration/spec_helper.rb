ced6e79b41caf9544bfcf42ac9a82f327e1635bf
Mike Conigliaro <mike@conigliaro.org>
2017-01-18 14:12:11 -0700

    Move ops_dev to its own repo

--- ops_dev/test/integration/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-require 'serverspec'
-
-set :backend, :exec
-
-ENV['TARGET_HOST'] = 'localhost' unless ENV['TARGET_HOST']
-ENV['TARGET_PORT'] = '4567' unless ENV['TARGET_PORT']
--- ops_dev/test/unit/test_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-require 'minitest/autorun'
-require 'rack/test'
-
-ENV['RACK_ENV'] = 'test'
-
-require File.expand_path('../../../app.rb', __FILE__)
